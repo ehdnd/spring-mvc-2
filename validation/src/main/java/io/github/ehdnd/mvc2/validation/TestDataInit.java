14badb15881c695e5496b9ba325a19efbf826bc2
ehdnd_mac <taeungjang@naver.com>
2026-09-24 20:16:08 +0900

    5.41 프로젝트 설정 V1

---
+++ validation/src/main/java/io/github/ehdnd/mvc2/validation/TestDataInit.java
@@ -0,0 +1,24 @@
+package io.github.ehdnd.mvc2.validation;
+
+import io.github.ehdnd.mvc2.validation.domain.item.Item;
+import io.github.ehdnd.mvc2.validation.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+  private final ItemRepository itemRepository;
+
+  /**
+   * 테스트용 데이터 추가
+   */
+  @PostConstruct
+  public void init() {
+    itemRepository.save(new Item("itemA", 10000, 10));
+    itemRepository.save(new Item("itemB", 20000, 20));
+  }
+
+}
---
+++ validation/src/test/java/io/github/ehdnd/mvc2/validation/message/MessageSourceTest.java
@@ -0,0 +1,52 @@
+package io.github.ehdnd.mvc2.validation.message;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Locale;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+@SpringBootTest
+public class MessageSourceTest {
+
+  @Autowired
+  MessageSource ms;
+
+  @Test
+  void helloMessage() {
+    String result = ms.getMessage("hello", null, null); // local 없으면 default 선택
+    assertThat(result).isEqualTo("안녕");
+  }
+
+  @Test
+  void notFoundMessageCode() {
+    assertThatThrownBy(() -> ms.getMessage("no_code", null, null)).isInstanceOf(
+        NoSuchMessageException.class);
+  }
+
+  @Test
+  void notFoundMessageCodeDefaultMessage() {
+    String res = ms.getMessage("no_code", null, "기본 메시지", null);
+    assertThat(res).isEqualTo("기본 메시지");
+  }
+
+  @Test
+  void argumentMessage() {
+    String res = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
+    assertThat(res).isEqualTo("안녕 Spring");
+  }
+
+  @Test
+  void defaultLang() {
+    assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
+    assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
+  }
+
+  @Test
+  void enLang() {
+    assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
+  }
+}
