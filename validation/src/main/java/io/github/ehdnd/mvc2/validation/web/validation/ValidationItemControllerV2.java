ed2da2eabbd8ca27fb3a9af74eb2724b8c74d624
ehdnd_mac <taeungjang@naver.com>
2026-09-25 18:17:30 +0900

    5.55 Validator 분리2
    스프링이 `Validator` 인터페이스를 제공한다.
    
    @Validated 를 사용하면 호출될때마다 설정한 `itemValidator` 를 먼저 사용한다.
    사용자가 호출할 때 마다 WebDataBinder 가 새로 만들어진다.

--- validation/src/main/java/io/github/ehdnd/mvc2/validation/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/io/github/ehdnd/mvc2/validation/web/validation/ValidationItemControllerV2.java
@@ -11,8 +11,10 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
-import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +29,11 @@ public class ValidationItemControllerV2 {
 
   private final ItemRepository itemRepository;
   private final ItemValidator itemValidator;
+
+  @InitBinder // 이 컨트롤러에만 적용된다.
+  public void init(WebDataBinder dataBinder) {
+    dataBinder.addValidators(itemValidator);
+  }
 
   @GetMapping
   public String items(Model model) {
@@ -222,11 +229,28 @@ public class ValidationItemControllerV2 {
   }
 
 
-  @PostMapping("/add")
+  //  @PostMapping("/add")
   public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
       RedirectAttributes redirectAttributes) {
 
     itemValidator.validate(item, bindingResult);
+
+    if (bindingResult.hasErrors()) {
+      log.info("bindingResult = {}", bindingResult);
+      return "validation/v2/addForm";
+    }
+
+    // 성공 로직
+    Item savedItem = itemRepository.save(item);
+    redirectAttributes.addAttribute("itemId", savedItem.getId());
+    redirectAttributes.addAttribute("status", true);
+    return "redirect:/validation/v2/items/{itemId}";
+  }
+
+
+  @PostMapping("/add")
+  public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult,
+      RedirectAttributes redirectAttributes) {
 
     if (bindingResult.hasErrors()) {
       log.info("bindingResult = {}", bindingResult);
