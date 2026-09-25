e1fe28f5d30223846f445ca08610ed83eb3b6148
ehdnd_mac <taeungjang@naver.com>
2026-09-25 18:36:28 +0900

    6.58 Bean Validation - 시작
    인터페이스: jakarta.validation-api
    구현체: hibernate-validator

--- validation/src/main/java/io/github/ehdnd/mvc2/validation/domain/item/Item.java
+++ validation/src/main/java/io/github/ehdnd/mvc2/validation/domain/item/Item.java
@@ -1,13 +1,25 @@
 package io.github.ehdnd.mvc2.validation.domain.item;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 public class Item {
 
   private Long id;
+
+  @NotBlank
   private String itemName;
+
+  @NotNull
+  @Range(min = 1000, max = 1_000_000)
   private Integer price;
+
+  @NotNull
+  @Max(9998)
   private Integer quantity;
 
   public Item() {
