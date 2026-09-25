92c298bbaa5c94ad886157aef7a7d61114f9b3c0
ehdnd_mac <taeungjang@naver.com>
2026-09-25 15:13:21 +0900

    5.43 검증 직접 처리 - 개발

--- validation/src/main/java/io/github/ehdnd/mvc2/validation/web/validation/ValidationItemControllerV1.java
+++ validation/src/main/java/io/github/ehdnd/mvc2/validation/web/validation/ValidationItemControllerV1.java
@@ -2,14 +2,19 @@ package io.github.ehdnd.mvc2.validation.web.validation;
 
 import io.github.ehdnd.mvc2.validation.domain.item.Item;
 import io.github.ehdnd.mvc2.validation.domain.item.ItemRepository;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/validation/v1/items")
 @RequiredArgsConstructor
@@ -33,12 +38,46 @@ public class ValidationItemControllerV1 {
 
   @GetMapping("/add")
   public String addForm(Model model) {
+    // validation) model을 넘겼기에 검증에 실패했을 때 데이터가 다시 보이도록 그대로 재사용이 가능하다.
     model.addAttribute("item", new Item());
     return "validation/v1/addForm";
   }
 
   @PostMapping("/add")
-  public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+  public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes,
+      Model model) {
+
+    // model에 검증 오류 결과 포함해야한다.
+    // 검증 오류 결과 보관
+    Map<String, String> errors = new HashMap<>();
+
+    // 검증 로직
+    if (!StringUtils.hasText(item.getItemName())) {
+      errors.put("itemName", "상품 이름은 필수입니다.");
+    }
+    if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+      errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+    }
+    if (item.getQuantity() == null || item.getQuantity() > 9999) {
+      errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+    }
+
+    // 특정 필드가 아닌 복합 룰 검증
+    if (item.getPrice() != null && item.getQuantity() != null) {
+      int resultPrice = item.getPrice() * item.getQuantity();
+      if (resultPrice < 10000) {
+        errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+      }
+    }
+
+    // 검증에 실패하면 다시 입력 폼으로
+    if (!errors.isEmpty()) {
+      log.info("errors = {}", errors);
+      model.addAttribute("errors", errors);
+      return "validation/v1/addForm";
+    }
+
+    // 성공 로직
     Item savedItem = itemRepository.save(item);
     redirectAttributes.addAttribute("itemId", savedItem.getId());
     redirectAttributes.addAttribute("status", true);
